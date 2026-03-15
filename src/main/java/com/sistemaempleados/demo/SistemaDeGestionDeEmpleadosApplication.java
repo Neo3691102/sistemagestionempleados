f74af5cc0fc362fad9e01f1b5faf861ff285727c
Neo3691102 <nestor8772@gmail.com>
2026-03-14 22:02:00 -0600

    Estructura inicial

---
+++ src/main/java/com/sistemaempleados/demo/SistemaDeGestionDeEmpleadosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaempleados.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeEmpleadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaempleados/demo/SistemaDeGestionDeEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaempleados.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDeEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
