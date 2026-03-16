839ea4165c64842977e4c988b62a56262d653bf5
Neo3691102 <nestor8772@gmail.com>
2026-03-16 12:18:43 -0600

    Repositorio y Servicio

---
+++ src/main/java/com/sistemaempleados/demo/Servicio/EmpleadoServiceImp.java
@@ -0,0 +1,49 @@
+package com.sistemaempleados.demo.Servicio;
+
+import com.sistemaempleados.demo.Entidades.Empleado;
+import com.sistemaempleados.demo.Repositorios.EmpleadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmpleadoServiceImp implements EmpleadoService{
+
+    @Autowired
+    private EmpleadoRepository empleadoRepository;
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Empleado> listarEmpleados() {
+        return empleadoRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Empleado> findAll(Pageable pageable) {
+        return empleadoRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional
+    public void save(Empleado empleado) {
+        empleadoRepository.save(empleado);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Empleado findOne(Long id) {
+        return empleadoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        empleadoRepository.deleteById(id);
+    }
+}
